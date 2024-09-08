f7e9ae1304878251c8d257e462ecef261b2f06d9
avareno <miguelcabral798@gmail.com>
2024-09-08 11:29:53 +0100

    configured swaylock

--- .config/hypr/swaylock/lock.sh
+++ .config/hypr/swaylock/lock.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 swaylock \
-  --color 282828 \
-  --indicator \
-  --image /path/to/image.png \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--ring-color ffffff \
+	--key-hl-color 888888 \
+	--line-color 000000 \
+	--inside-color 000088 \
+	--separator-color 000000 \
+  --key-hl-color 888888 \
+  --line-color 000000 \
+  --inside-color 000000 \
+  --ring-ver-color 00ff00 \
+  --inside-ver-color 00ff00 \
+  --separator-color 000000 \
+  --text-color ffffff \
