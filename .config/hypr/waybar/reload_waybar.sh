507ab2b212604827fb011b12a26cea0614481f6e
avareno <miguelcabral798@gmail.com>
2024-09-23 14:09:35 +0100

    changes in files organization

--- .config/hypr/waybar/reload_waybar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-killall waybar 
-
-
-waybar -c ~/configuration/.config/hypr/waybar/config.jsonc & -s ~/configuration/.config/hypr/waybar/stylecss
---
+++ .config/waybar/reload_waybar.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+killall waybar 
+
+
+waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
