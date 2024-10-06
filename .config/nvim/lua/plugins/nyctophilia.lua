b89063430a7de54cadba1fff2f026f1cc9f74298
avareno <miguelcabral798@gmail.com>
2024-10-06 11:57:48 +0100

    changes in hyprland.conf, added redshift filter, added dark mode to qt5a nd styled waybar

---
+++ .config/nvim/lua/plugins/nyctophilia.lua
@@ -0,0 +1,8 @@
+return { 
+    'javiorfo/nvim-nyctophilia',
+    lazy = false,
+    init = function()
+        -- Available themes: nox, umbra, nebula and tenebra
+        vim.cmd[[colorscheme umbra]]
+    end
+}
