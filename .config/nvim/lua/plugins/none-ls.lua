7d7cc69e169feac6561b2cf8cb34e3d847ed92da
avareno <miguelcabral798@gmail.com>
2025-06-30 17:17:00 +0100

    Changes flutter nvim

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,19 +1,19 @@
 -- Formatters
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				-- null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.formatting.sql_formatter,
-				null_ls.builtins.formatting.isort,
-				-- null_ls.builtins.formatting.eslint_d,
-				-- null_ls.builtins.formatting.ast_grep,
-			},
-		})
-		vim.keymap.set({ "n", "i", "v" }, "<A-S-f>", vim.lsp.buf.format, {})
-	end,
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettierd,
+                null_ls.builtins.formatting.sql_formatter,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.ast_grep,
+                -- null_ls.builtins.formatting.clang_format,
+                -- null_ls.builtins.formatting.eslint_d,
+            },
+        })
+        vim.keymap.set({ "n", "i", "v" }, "<A-S-f>", vim.lsp.buf.format, {})
+    end,
 }
